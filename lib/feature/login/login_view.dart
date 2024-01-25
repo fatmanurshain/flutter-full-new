926deb02bbf62c6570809cb4f278716d0fb4f670
Fatmanur Şahin <fatmanursahin@192.168.1.68>
2024-01-25 14:00:20 +0300

    first commit

---
+++ lib/feature/login/login_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: context.padding.low,
+        child: Column(),
+      ),
+    );
+  }
+}
---
+++ lib/product/enum/image_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+enum IconConstants {
+  microphone('ic_microphone');
+
+  final String value;
+  // ignore: sort_constructors_first
+  const IconConstants(this.value);
+
+  String get toPng => 'assets/images/$value.png';
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(IconConstants.microphone.toPng);
+  }
+}
